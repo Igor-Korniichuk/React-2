7906167098c15fee783ead90091e6dc158982b4e
Igor-Korniichuk <kiv160780@gmail.com>
2022-08-30 13:47:39 +0300

    Использование события

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,13 +3,29 @@ import styles from "./Counter.module.css";
 
 
 class Counter extends React.Component {
+    handleEncrement = (event) => {
+        console.log("кликнули в увеличить");
+
+        const target = event.target;  
+        // для получения event.target в асинхронном коде записываем в переменную
+        
+        setTimeout(() => { console.log(target) }, 1000);
+    }
+
+    handleDecrement = () => {
+        console.log("кликнули в уменшить");
+        console.log(this);
+    }
+
+
+
     render() {
         return <div className={styles.Counter}>
                     <span className={styles.Counter__value}>0</span>
 
                     <div className="Counter__controls">
-                <button type="button" onClick={() => {console.log("кликнули в увеличить");}}>Увеличить на 1</button>
-                        <button type="button">Уменшить на 1</button>
+                        <button type="button" onClick={this.handleEncrement}>Увеличить на 1</button>
+                        <button type="button" onClick={this.handleDecrement}>Уменшить на 1</button>
                     </div>
                 </div>
     }
