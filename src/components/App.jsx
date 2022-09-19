a38f60da87c7c858aedea0c1527c49c347ff3fe7
Igor-Korniichuk <kiv160780@gmail.com>
2022-09-19 09:48:02 +0300

    Выполняет туму 2 по Репете А.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,91 @@
 import React, {Component} from "react";
 import Counter from "./Counter/Counter";
 import TodoList from "./TodoList";
+import TodoEditor from "./TodoEditor";
+import initialTodos from "../todos.json";
+import shortid from "shortid";
+import Filter from "./Filter";
 
 
 class App extends Component {
   state = {
-    todos: [
-      { id: 'id-1', text: 'Выучить основы React', completed: 'false' },
-      { id: 'id-2', text: 'Разобраться с React Router', completed: 'false' },
-      { id: 'id-3', text: 'Пережить Redux', completed: 'false' },      
-    ], 
+    todos: initialTodos, 
+    filter: '',
   };
+
+  addTodo = text => {    
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
+
+    this.setState(({todos}) => ({
+      todos: [todo, ...todos],
+    }))
+  }
+
+  deleteTodo = (todoId) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }))
+  }
+
+  toggleCompleted = todoId => {
+    console.log(todoId);
+
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoId) {
+    //       console.log('Нашли тот туду, который нужно!')
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       };
+    //     }
+
+    //     return todo;
+    //   }),
+    // }))
+
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo => todo.id === todoId ? { ...todo, completed: !todo.completed, } : todo,),
+    }));
+  };
+
+  changeFilter = (event) => {
+    this.setState({filter: event.currentTarget.value});
+  }
+
+  getVisibleTodos = () => {
+  const { filter, todos } = this.state;
+  const normalizedFilter = filter.toLowerCase();
+
+  return todos.filter(todo => todo.text.toLowerCase().includes(normalizedFilter),)
+  }
+  
   render() {
-    const { todos } = this.state;
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+  const complitedTodosCount = todos.reduce((total, todo) => (todo.completed ? total + 1 : total), 0);
+  const visibleTodos = this.getVisibleTodos();
     return (
     <div>
-      <Counter initialValue={110} />
-        <TodoList todos={todos} />
+        <Counter initialValue={110} />
+
+        <div>
+          <p>Общее количество: {totalTodoCount}</p>
+          <p>Количество выполненных: {complitedTodosCount}</p>
+        </div>
+
+        <TodoEditor onSubmit={this.addTodo} /> 
+        <Filter value={filter} onChange={ this.changeFilter} />
+
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
     </div>
   );
   }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from './TodoList.module.css';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
     <ul className={styles.TodoList}>
-        {todos.map(({ id, text }) => (
-            <li key={id} className={styles.TodoList__item}>
+        {todos.map(({ id, text, completed }) => (
+            <li
+                key={id} className={styles.TodoList__item}
+            >
+                <input type="checkbox"
+                    className={styles.TodoList__checkbox}
+                    checked={completed}
+                    onChange={() => onToggleCompleted(id)}
+                />
                 <p className={styles.TodoList__text}>{text}</p>
-            <button>Удалить</button>
+                <button
+                    type="button"
+                    className={styles.TodoList__btn}
+                    onClick={() => onDeleteTodo(id)}>
+                    Удалить
+                </button>
             </li>
         ))}
     </ul>
